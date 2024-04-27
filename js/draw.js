c32cb50d9feaacb8471131f0eaa5433240785f9d
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-04-27 09:08:14 +0000

    draw on canvas

---
+++ js/draw.js
@@ -0,0 +1,12 @@
+const draw = {}
+
+draw.path = function (ctx, path, color="black") {
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 3;
+    ctx.beginPath();
+    ctx.moveTo(...path[0]);
+    for(let i=1; i<path.length; i++) {
+        ctx.lineTo(...path[i]);
+    }
+    ctx.stroke();
+}
